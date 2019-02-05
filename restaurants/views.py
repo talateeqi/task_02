d635aa422616c57dbdc3e3bfa7df8a6ea48524da
Thuraya AlAteeqi <ThurayaAlAteeqi@Thurayas-MacBook-Air.local>
2019-02-05 12:05:08 +0300

    submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def genzapp(request):
+	context = {
+			'msg' : 'Hello World'
+	}
+	return render(request, "Task02.html", context)
--- task_02/urls.py
+++ task_02/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from restaurants import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('Thuraya',views.genzapp ,name='hello-world'),
 ]
